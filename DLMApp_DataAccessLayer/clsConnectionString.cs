53640ae630e68b51b2472f9b0c33fbae348f20b7
Ebrahim Hasan <ebrahim-hasan-dev@gmail.com>
2026-05-18 14:22:43 +0300

    Add a comment explaining the topic of database server security and best practices

--- DLMApp_DataAccessLayer/clsConnectionString.cs
+++ DLMApp_DataAccessLayer/clsConnectionString.cs
@@ -4,7 +4,15 @@ namespace DLMApp_DataAccessLayer
 {
     internal class clsConnectionString
     {
+        // This connection string contains a test password, not the real one, as it was changed after the project was completed.
+        
         //public static string ConnectionString = "Server=.;DataBase=MyDLM;User ID =sa;Password=123456;";
+
+        // The best practice here is to use the Windows Authentication method already implemented in the project code,
+        // or to use the Environment Variables method with the dotenv.net library to read variables from the .env text file and
+        // write the name of this file inside the .gitignore file so that it is not uploaded to GitHub.
+
+
         public static string ConnectionString = "Server=.;DataBase=MyDLM;Integrated Security=True;";
     }
 }
